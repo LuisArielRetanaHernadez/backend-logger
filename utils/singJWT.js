02741ce262e4d5eb8bf25945d4f6c63e17c10a50
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-03-29 20:17:11 -0600

    creando un utils para firmar jwt

---
+++ utils/singJWT.js
@@ -0,0 +1,9 @@
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+exports.singJWT = (data, expiresIn) => {
+  const expiresInIs =  expiresIn || process.env.JWT_EXPIRES_IN;
+  
+  jwt.sign({data}, process.env.JWT_SECRET, {expiresIn: expiresInIs})
+}
