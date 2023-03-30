4228ab6a797523ad5b112cf9d98f15358f178b4f
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-03-30 10:44:15 -0600

    creando middleware para la protecion de req por id

--- middlewares/authorizations.middleware.js
+++ middlewares/authorizations.middleware.js
@@ -17,6 +17,28 @@ class AuthorizationService {
     return next()
 
   }
+
+  async protectId(req, res, next) {
+    const { id } = req.params
+    const idToken = req.currentToken._id
+    
+    if (id !== idToken) {
+      return res.status(401).json({
+        message: 'invalid credentials'
+      })
+    }
+
+    const aplicationFind = await Authorizations.findById(id)
+
+    if (!aplicationFind) {
+      return res.status(404).json({
+        message: 'invalid credentials'
+      })
+    
+    }
+
+    return next()
+  }
 }
 
 module.exports = new AuthorizationService()
