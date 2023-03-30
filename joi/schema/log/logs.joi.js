cf97b7d1e8aa2b8f2d7f43328c2ae26c9cb27e48
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-03-30 12:55:34 -0600

    creando el schema joi logs

---
+++ joi/schema/log/logs.joi.js
@@ -0,0 +1,11 @@
+const joi = require('joi');
+
+const logsJoi = joi.object({
+  type: joi.any.valid('error', 'warning', 'info' ).required(),
+  priority: joi.any.valid('lowest', 'low', 'medium', 'high', 'highest').required(),
+  path:  joi.string().required(),
+  message: joi.any().required(),
+  response: joi.any().required(),
+})
+
+exports.logsJoi = logsJoi;
