901df3ca4d84b3aab812cbd331e66b1a2b81cedf
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-03-30 15:27:19 -0600

    agregando el compo request

--- joi/schema/log/logs.joi.js
+++ joi/schema/log/logs.joi.js
@@ -1,10 +1,11 @@
 const joi = require('joi');
 
 const logsJoi = joi.object({
-  type: joi.any.valid('error', 'warning', 'info' ).required(),
-  priority: joi.any.valid('lowest', 'low', 'medium', 'high', 'highest').required(),
+  type: joi.any().valid('error', 'warning', 'info' ).required(),
+  priority: joi.any().valid('lowest', 'low', 'medium', 'high', 'highest').required(),
   path:  joi.string().required(),
   message: joi.any().required(),
+  request: joi.any().required(),
   response: joi.any().required(),
 })
 
