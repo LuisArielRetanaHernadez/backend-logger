27a226ee6606d8b4438cedaeb95f4f5f8a56146f
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-03-29 19:58:35 -0600

    agregando la funcion connDB a la configuracion del server

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const app = require('./app')
 
+const connDB = require('./utils/database')
+
 const startServer = () => {
   const PORT = 3434
 
   app.listen(PORT, () => {
+    connDB()
     console.log('server start in PORT ', PORT)
   })
 }
