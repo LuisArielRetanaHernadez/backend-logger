84c1103c1d9618ff4293d7e525a8a9a23331cadb
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-03-30 23:07:45 -0600

    agregando AppError y tryCatch

--- middlewares/authJWT.js
+++ middlewares/authJWT.js
@@ -1,30 +1,28 @@
-// singJWT
+// utils
 const { verifyJWT } = require('../utils/singJWT')
+const AppError = require('../utils/AppError');
+const { tryCatch } = require('../utils/tryCatch');
 
-exports.authJWT = async (req, res, next) => {
-  try {
-    let token = null;
+exports.authJWT = tryCatch( async(req, res, next) => {
+  let token = null;
     
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-      token = req.headers.authorization.split(' ')[1]
-    } else {
-      return res.status(401).json({ message: 'invalid credentials' })
-    }
-
-    const tokenIs = verifyJWT(token)
-
-    if (!tokenIs) {
-      return res.status(401).json({ message: 'invalid credentials' })
-    }
-
-    req.currentAplication = {
-      ...tokenIs,
-      token
-    }
-
-    return next()
-
-  } catch (error) {
-    return res.status(401).json({ message: 'invalid credentials' })
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1]
+  } else {
+    return next(new AppError('invalid credentials', 401))
   }
-}
+
+  const tokenIs = verifyJWT(token)
+
+  if (!tokenIs) {
+    return next(new AppError('invalid credentials', 401))
+  }
+
+  req.currentAplication = {
+    ...tokenIs,
+    token
+  }
+
+  return next()
+
+})
