9cd4ccc26c782a80dffe1436f66791674d6e17d2
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-03-30 20:12:36 -0600

    creando los campos created_at y updated_at y posterior inmutando el campo created_at

--- schema/authorizations.schema.js
+++ schema/authorizations.schema.js
@@ -4,12 +4,21 @@ const Schema = mongoose.Schema;
 const authorizationModel = new Schema({
   application_id: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'application',
+    ref: 'aplications',
     required: true,
   },
   token: {
     type: String,
   },
+  created_at: {
+    type:  Date,
+    default: Date.now,
+    inmutable: true
+  },
+  updated_at: {
+    type:  Date,
+    default: Date.now
+  }
 })
 
 const authorization = mongoose.model('authorization', authorizationModel);
