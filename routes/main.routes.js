7f9bad5d73111b66968f88da7484f361c8cba231
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-03-30 22:59:17 -0600

    exportando tryCatch y AppError y poniendolo en lso controller

--- routes/main.routes.js
+++ routes/main.routes.js
@@ -14,13 +14,16 @@ const { validator } = require('../joi/validator.joi')
 const { aplicationsJoi } = require('../joi/schema/log/aplications.joi')
 const { logsJoi } = require('../joi/schema/log/logs.joi')
 
-router.get(`/${prefix}/`, controller.all);
-router.post(`/${prefix}/`, validator(aplicationsJoi), controller.create);
-router.post(`/${prefix}/create`, authJWT, AuthorizationService.protect, validator(logsJoi), controller.createLog);
+// utils
+const { tryCatch } = require('../utils/tryCatch')
 
-router.get(`/${prefix}/:id`, authJWT, AuthorizationService.protect, AuthorizationService.protectLogById, controller.info);
-router.put(`/${prefix}/:id`, authJWT, AuthorizationService.protect, AuthorizationService.protectLogById, controller.update);
-router.delete(`/${prefix}/:id`, authJWT, AuthorizationService.protect, AuthorizationService.protectLogById, controller.delete);
+router.get(`/${prefix}/`, tryCatch(controller.all));
+router.post(`/${prefix}/`, validator(aplicationsJoi), tryCatch(controller.create));
+router.post(`/${prefix}/create`, authJWT, AuthorizationService.protect, validator(logsJoi), tryCatch(controller.createLog));
+
+router.get(`/${prefix}/:id`, authJWT, AuthorizationService.protect, AuthorizationService.protectLogById, tryCatch(controller.info));
+router.put(`/${prefix}/:id`, authJWT, AuthorizationService.protect, AuthorizationService.protectLogById, tryCatch(controller.update));
+router.delete(`/${prefix}/:id`, authJWT, AuthorizationService.protect, AuthorizationService.protectLogById, tryCatch(controller.delete));
 
 router.post(`/${prefix}/login`, validator(aplicationsJoi), controller.login);
 
