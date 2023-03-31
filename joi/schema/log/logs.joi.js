455bc328cfbb80de17295e3823dec69a4a23b1d8
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-03-30 20:07:25 -0600

    quitando el requiered en los campos de logs.joi.js

--- joi/schema/log/logs.joi.js
+++ joi/schema/log/logs.joi.js
@@ -1,12 +1,12 @@
 const joi = require('joi');
 
 const logsJoi = joi.object({
-  type: joi.any().valid('error', 'warning', 'info' ).required(),
-  priority: joi.any().valid('lowest', 'low', 'medium', 'high', 'highest').required(),
-  path:  joi.string().required(),
-  message: joi.any().required(),
-  request: joi.any().required(),
-  response: joi.any().required(),
+  type: joi.any().valid('error', 'warning', 'info' ),
+  priority: joi.any().valid('lowest', 'low', 'medium', 'high', 'highest'),
+  path:  joi.string(),
+  message: joi.any(),
+  request: joi.any(),
+  response: joi.any(),
 })
 
 exports.logsJoi = logsJoi;
