c9d1d1c361a59e02159fedeb4217f9b5c2cf531b
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-03-30 20:33:30 -0600

    separando los routers

--- routes/main.routes.js
+++ routes/main.routes.js
@@ -17,9 +17,11 @@ const { logsJoi } = require('../joi/schema/log/logs.joi')
 router.get(`/${prefix}/`, controller.all);
 router.post(`/${prefix}/`, validator(aplicationsJoi), controller.create);
 router.post(`/${prefix}/create`, authJWT, AuthorizationService.protect, validator(logsJoi), controller.createLog);
+
 router.get(`/${prefix}/:id`, authJWT, AuthorizationService.protect, AuthorizationService.protectLogById, controller.info);
 router.put(`/${prefix}/:id`, authJWT, AuthorizationService.protect, AuthorizationService.protectLogById, controller.update);
 router.delete(`/${prefix}/:id`, authJWT, AuthorizationService.protect, AuthorizationService.protectLogById, controller.delete);
+
 router.post(`/${prefix}/login`, validator(aplicationsJoi), controller.login);
 
 module.exports = router;
